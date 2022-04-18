c28b87967d7e4d0d9b0e14235191b661ff15a545
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-18 13:10:32 -0400

    first commit. Created basic HTML layout and CSS styling with data fetching from API

---
+++ index.js
@@ -0,0 +1,38 @@
+const filterInput = document.getElementById('filter')
+filterInput.addEventListener('keyup', fetchAutoComplete)
+
+const cardList = document.querySelector('.cardlist')
+
+//Pass user input through autocomplete API to generate 20 cards
+function fetchAutoComplete() {
+    cardList.innerHTML = ``;
+    fetch(`https://api.scryfall.com/cards/autocomplete?q=${filterInput.value}`)
+    .then(res => res.json())
+    .then(res => getCardImages(res.data))
+    .then(sleeper(100))
+};
+
+//Get the image of each card generated
+function getCardImages(cards) {
+    cards.forEach(element => {
+        fetch(`https://api.scryfall.com/cards/named?fuzzy=${element}`)
+        .then(res => res.json())
+        .then(card => {
+            //createImages(card.name, card['image_uris'].small)
+            let img = document.createElement("img");
+            img.src = card['image_uris'].small;
+            img.className = "cardImage";
+            img.artist = card.artist;
+            img.oracleText = card.orcale_text;
+            cardList.append(img);
+        })
+        .then(sleeper(100))
+    })
+};
+
+//API Delay
+function sleeper(ms) {
+    return function(x) {
+      return new Promise(resolve => setTimeout(() => resolve(x), ms));
+    };
+  }
