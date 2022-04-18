91b31949e113895b713f83f09f2ccc35988704df
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-18 14:55:27 -0400

    git bran

--- index.js
+++ index.js
@@ -18,21 +18,34 @@ function getCardImages(cards) {
         fetch(`https://api.scryfall.com/cards/named?fuzzy=${element}`)
         .then(res => res.json())
         .then(card => {
+            console.log(card)
             //createImages(card.name, card['image_uris'].small)
             let img = document.createElement("img");
+            img.name = card.name;
+            img.colors = card.colors;
+            img.flavorText = card.flavor_text;
+            img.keywords = card.keywords;
             img.src = card['image_uris'].small;
             img.className = "cardImage";
             img.artist = card.artist;
             img.oracleText = card.orcale_text;
+            img.price = card.prices.usd;
+            img.addEventListener("click", (e) => handleClick(e, card));
             cardList.append(img);
+
         })
         .then(sleeper(100))
     })
 };
+
+function handleClick(e) {
+    console.log(e.target)
+}
 
 //API Delay
 function sleeper(ms) {
     return function(x) {
       return new Promise(resolve => setTimeout(() => resolve(x), ms));
     };
-  }
+}
+
